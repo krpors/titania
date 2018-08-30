745e0b774bca86e8fc1274303a65eb6db32186bc
Kevin Pors <krpors@gmail.com>
2018-08-30 21:46:08 +0200

    add debug_print
    Macro is tied to the NDEBUG macro.

--- util.h
+++ util.h
@@ -16,4 +16,11 @@ struct point {
  */
 bool is_color_equal(SDL_Color* one, SDL_Color* two);
 
+#ifndef NDEBUG
+#define debug_print(fmt, ...) \
+            do { fprintf(stderr, "\x1b[34;1m%s\x1b[0m:%d:\x1b[32m%s()\x1b[0m    " fmt, __FILE__, __LINE__, __func__, __VA_ARGS__); } while (0)
+#else
+#define debug_print(fmt, ...) (void)0;
+#endif // NDEBUG
+
 #endif // UTIL_H
